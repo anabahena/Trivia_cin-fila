0b14a55dda31d73163a5112aeeed51d75b8a86d6
Guadalupe González López <Guadalupe@MacBook-Air-de-Guadalupe.local>
2019-06-05 16:19:08 -0500

    Update app.js
    Se agregaron id a los botones de la primera pregunta de la categoria de fantasia y terror

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const buttonFantasy = document.getElementById("button-fantasy");
 const buttonTerror = document.getElementById("button-terror");
 const cuestionOneFantasy = document.getElementById("cuestion1-fantasy");
 const cuestionOneTerror = document.getElementById("cuestion1-terror");
+const buttonAfantasy= document.getElementById ("buttonA-fantasy")
 
 const getName = () =>{
   primerSection.classList.add("desappear");
@@ -15,13 +16,13 @@ const getName = () =>{
 
 }
 const getCuestionFantasy = () =>{
-  cuestionOneFantasy.innerHTML = "1. ¿En qué año se estreno la película La historia sin fin? <br/><button>A. 1987</button><br><button>B. 2008</button><br/><button> C. 1990</button>"
+  cuestionOneFantasy.innerHTML = "1. ¿En qué año se estreno la película La historia sin fin? <br/><button id='buttonA-fantasy'>A. 1987</button><br><button id= 'buttonB-fantasy'>B. 2008</button><br/><button 'buttonC-fantasy'> C. 1990</button>"
 
 }
 
 
 const getCuestionTerror = () =>{
-  cuestionOneTerror.innerHTML = "1. ¿Cuál de estas películas pertenece al género de Terror? <br/><button id='buttonResA'>A. Monster Inc.</button> <br/><button>B. El resplandor</button><br/><button>C. Frankewinie</button>"
+  cuestionOneTerror.innerHTML = "1. ¿Cuál de estas películas pertenece al género de Terror? <br/><button id='buttonA-Terror'>A. Monster Inc.</button> <br/><button id='buttonB-fantasy'>B. El resplandor</button><br/><button id='buttonC-fantasy'>C. Frankewinie</button>"
 }
 
 
